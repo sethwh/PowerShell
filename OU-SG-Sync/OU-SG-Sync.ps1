a8b99949c5cb5f39c6cc63465cd20131a742ff12
Seth Hearron <sethhearron@gmail.com>
2020-05-27 15:29:21 -0500

    Added folder

---
+++ OU-SG-Sync/OU-SG-Sync.ps1
@@ -0,0 +1,23 @@
+﻿# Variables
+$OU = "OU=Users,OU=Dallas,OU=Texas,DC=Contoso,DC=local"
+$SG = "TestGroupTest"
+
+
+$OUMembers = Get-ADUser -SearchBase $OU -Filter * -ResultSetSize 1000 | Select SamAccountName
+
+Foreach ($User in  $OUMembers) {
+
+     
+     Write-Output $User.SamAccountName
+     $GroupMembers = Get-ADGroupMember -Identity $SG -Recursive | Select SamAccountName
+     
+        If ($GroupMembers.SamAccountName -contains $User.SamAccountName) {
+                Write-Host "$User is already member of $SG"
+            } 
+        Else {
+                Write-Host "Adding User: $User"
+                Add-ADGroupMember -Identity "TestGroupTest" -Members $User
+       
+             }
+    
+    }
